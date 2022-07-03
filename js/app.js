592152c1db1a7f379a91b40406198017b0023392
Juan <[judagosa8520@gmail.com]>
2022-07-03 12:41:48 -0500

    Llenar las opciones con los años

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
 //Constructores
-
 function Seguro(marca, year, tipo) {
-    this.marca = marca,
-    this.year = year, 
-    this.tipo = tipo
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
 }
 
+function UI() {}
+
+
+// Llena las opciones de los años 
+UI.prototype.llenarOpciones = () => {
+    const max = new Date().getFullYear();
+          min = max - 20;
+
+    const selectYear  = document.querySelector('#year');
+
+    for (let i = max ; i > min; i--) {
+        let option = docment.createElement('option');
+        option.value = i;
+        option.textContent = i;
+        selectYear.appendChild(option);
+    }
+}
+
+// Instanciar UI
+const ui = new UI();
+
+document.addEventListner('DOMContentLoaded', () => {
+    ui.llenarOpciones() // Llena el select con los años
+})
