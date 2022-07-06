750a3ef4961e744241535b6a5d90877f80153a28
Juan <[judagosa8520@gmail.com]>
2022-07-06 08:31:51 -0500

    Mostrar alertas en la pantalla

--- js/app.js
+++ js/app.js
@@ -23,6 +23,29 @@ UI.prototype.llenarOpciones = () => {
     }
 }
 
+// Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+    const div = document.createElement('div');
+
+    if(tipo === 'error') {
+        div.classList.add('error');
+    } else {
+        div.classList.add('correcto')
+    }
+
+    div.classList.add('mensaje', 'mt-10');
+    div.textContent = mensaje;
+
+    // Insertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
+
+}
+
 // Instanciar UI
 const ui = new UI();
 
@@ -50,9 +73,10 @@ function cotizarSeguro(e) {
     const tipo = document.querySelector('input[name="tipo"]:checked').value
     
     if(marca === '' || year === '' || tipo === '') {
-        console.log('No pasó la validacion');
-    } else {
-        console.log('Si pasó la validacion');
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return
     }
+
+    ui.mostrarMensaje('Cotizando...', 'exito');
 
 }
